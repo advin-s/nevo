7c27c593cb1fb50adf754d8a481cf5902109e0fd
advin-s <adithpyscho@gmail.com>
2025-01-25 22:53:06 +0530

    added token auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,32 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { inject } from '@angular/core';
+import { TokenService } from './services/auth/token.service';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { map } from 'rxjs';
+
+const isAuth:CanMatchFn = (route,segments) =>{
+    const tokenService = inject(TokenService)
+    const router = inject(Router)
+     return tokenService.token.pipe(map(token => 
+     {
+        if(token){
+            return true
+        }
+        else return new RedirectCommand(router.parseUrl(''))
+     }
+     ))
+}
 
 export const routes: Routes = [
     {
         path:'',
         component:LoginComponent
+    },
+    {
+        path:'welcome',
+        component:WelcomeComponent,
+        canMatch:[isAuth]
+
     }
 ];
